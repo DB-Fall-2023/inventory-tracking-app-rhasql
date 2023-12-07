13090d0e1be45ecfd1b7f110639c1aa4e25c60b0
Adrian Irizarry <adrian.irizarry@upr.edu>
2023-12-06 21:00:00 -0400

    testing

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from handler.itHandler import ITHandler
 from handler.otHandler import OTHandler
 from handler.exchangeHandler import ExchangeHandler
 from subprocess import Popen
+import os
 import subprocess
 import time
 
@@ -297,15 +298,18 @@ def getAllPrice():
      if request.method == 'GET':
          #Popen(['voila', '--port=8862','--no-browser','JupyterNotebooks/partPrice.ipynb'])
 
-         command = ['voila', '--port=8862', '--no-browser', 'JupyterNotebooks/partPrice.ipynb', '--Voila.tornado_settings={"headers": {"Content-Security-Policy": "frame-ancestors *"}}']
-         Popen(command)
+         #command = ['voila', '--port=8862', '--no-browser', 'JupyterNotebooks/partPrice.ipynb', '--Voila.tornado_settings={"headers": {"Content-Security-Policy": "frame-ancestors *"}}']
+         #Popen(command)
          #print(Popen(command))
-         return render_template('test.html')
+         heroku_app_url = f"https://{os.environ['HEROKU_APP_NAME']}.herokuapp.com"
+         voila_url = f"{heroku_app_url}:8866/"
+         return render_template('test.html', voila_url=voila_url)
 #     else: #catches any other methods
 #         return jsonify(Error="Method not allowed"), 405
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
 
 
 
